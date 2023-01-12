cd729b21c142b7083c17a5f5bff56d1c45ad9178
coucoseth <ochseth04@gmail.com>
2023-01-12 12:08:56 +0300

    Initial Commit

---
+++ testIos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testIos
+//
+//  Created by seth on 12/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testIos/testIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  testIosApp.swift
+//  testIos
+//
+//  Created by seth on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct testIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
